e6e6f1e40e70f05f4bd0c76fe22bba93fd9a0e8f
nirmala-mist <nirmala@mist.com>
2018-02-18 11:21:56 -0800

    Adding more details

--- SwiftTutorial.playground/Contents.swift
+++ SwiftTutorial.playground/Contents.swift
@@ -3,3 +3,20 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var str2 = "abc"
+let num : Int = 3 //Constant
+print(num)
+
+var num1 = 6
+var num2 = 12
+
+print(num1 + num)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+
+sqrt(2.0)
+Double.pi / 2
+max(sqrt(2.0), Double.pi / 2)
